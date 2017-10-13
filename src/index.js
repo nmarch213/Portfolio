6429050ba2933c747ec0789ac1c666484de40a43
Nicholas March <n.march213@gmail.com>
2017-10-12 23:14:07 -0500

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
